7e1a9d107c1389bea544559eb5ef4bfed8108106
Bhavya Bharti <bhavyab@outlook.com>
2017-08-14 13:31:10 -0700

    state

--- src/main.js
+++ src/main.js
@@ -2,15 +2,41 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+class Navbar extends React.Component{
+  render(){
+    return (
+          <header className="nav">
+             <h1>Counter</h1>
+          </header>
+    )
+  }
+}
+
 class App extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      title: 'state title',
+      count: 0,
+    }
+    this.addCount = this.addCount.bind(this)
+  }
+
+  addCount(e){
+    this.setState(state => {
+      return {
+        count: state.count + 1,
+      }
+    })
+
   }
   render(){
     return (
         <div>
-           <h1>Hello React</h1>
-           <p> Try to improve </p>
+        <Navbar  />
+           <h6>Hello React Component {this.state.title}</h6>
+           <p> Counter: {this.state.count} </p>
+           <button onClick={this.addCount}>new count state : {this.state.count}</button>
         </div>
     )
   }
